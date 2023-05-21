b7bb57d9a88330d58de911bef48777015a14acf7
pottsaf <aaronfpotts@gmail.com>
2023-05-21 14:28:01 -0600

    Finish navbar layout

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # restaurant
+
+## The aim of this is to build modules and components using the webpack bundler, so that it would be a simple matter of swapping out or modifying the component as desired.
+
 - [x] Base Layout
 - [ ] Components
     - [ ] Navbar
@@ -10,10 +13,22 @@
         - [ ] Interactivity
             - [ ] Active underline
             - [ ] Hover
+        - [ ] Secondary Navbar dropdown
+            - [ ] Menu
+                - [ ] Track where the user is scrolling and display on navbar
+            - [ ] Order
     - [ ] Pages
         - [ ] Main
         - [ ] Menu
+            - [ ] Submenu
+                - [ ] Drinks
+                - [ ] Apps
+                - [ ] Food
+                - [ ] Dessert
         - [ ] Order
+            - [ ] Call
+            - [ ] Online
+            
         - [ ] About
         - [ ] Contact
     - [ ] Footer
--- src/index.js
+++ src/index.js
@@ -7,20 +7,48 @@ function component() {
   const header = document.createElement('div')
   const content = document.createElement('div')
   const footer = document.createElement('div')
-  const btn = document.createElement('button')
-
+  const btnMain = document.createElement('button')
+  const btnMenu = document.createElement('button')
+  const btnOrder = document.createElement('button')
+  const btnAbout = document.createElement('button')
+  
   body.classList.add('body')
   header.classList.add('navbar')
   content.classList.add('content')
   footer.classList.add('footer')
-  btn.classList.add('navbtn')
+  btnMain.classList.add('navbtn')
+  btnMenu.classList.add('navbtn')
+  btnOrder.classList.add('navbtn')
+  btnAbout.classList.add('navbtn', 'about')
 
-  btn.innerHTML = 'Rando'
+  btnMain.innerHTML = 'Main'
+  btnMenu.innerHTML = 'Menu'
+  btnOrder.innerHTML = 'Order'
+  btnAbout.innerHTML = 'About'
   header.innerHTML = 'Nav'
   content.innerHTML = 'Content'
   footer.innerHTML = 'Footer'
 
-  header.appendChild(btn)
+  function showMain () {
+    content.innerHTML = 'Main page'
+  }
+
+  function showMenu () {
+    content.innerHTML = ''
+    const secondNav = document.createElement('div')
+    secondNav.classList.add('navbar')
+    content.appendChild(secondNav)
+    secondNav.innerHTML = 'Second nav'
+    content.innerHTML += 'Second page'
+  }
+
+  btnMain.addEventListener('click', showMain)
+  btnMenu.addEventListener('click', showMenu)
+
+  header.appendChild(btnMain)
+  header.appendChild(btnMenu)
+  header.appendChild(btnOrder)
+  header.appendChild(btnAbout)
   body.appendChild(header)
   body.appendChild(content)
   body.appendChild(footer)
