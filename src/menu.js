a6af1e835e81b601817ccd6ea69f066d707200a8
pottsaf <aaronfpotts@gmail.com>
2023-05-23 14:07:10 -0600

    Clarify menu listings

--- src/menu.js
+++ src/menu.js
@@ -5,8 +5,8 @@ const appBtn = document.createElement('button')
 const entreesBtn = document.createElement('button')
 const dessertsBtn = document.createElement('button')
 secondNav.classList.add('sidebar')
-appBtn.classList.add('menubutton')
-entreesBtn.classList.add('menubutton')
+appBtn.classList.add('menubutton', 'page-link')
+entreesBtn.classList.add('menubutton', 'page-link')
 dessertsBtn.classList.add('menubutton')
 
 insert.appendChild(secondNav)
@@ -18,9 +18,17 @@ secondNav.appendChild(entreesBtn)
 secondNav.appendChild(dessertsBtn)
 
 const wrapper = document.createElement('div')
-const apps = document.createElement('ol')
-const entrees = document.createElement('ol')
-const desserts = document.createElement('ol')
+const apps = document.createElement('ul')
+const appsList = document.createElement('li')
+const entrees = document.createElement('ul')
+const entreesList = document.createElement('li')
+const desserts = document.createElement('ul')
+const dessertsList = document.createElement('li')
+
+appsList.textContent = 'Apps list goes here'
+entreesList.textContent = 'Entrees list goes here'
+dessertsList.textContent = 'Desserts list goes here'
+
 
 wrapper.classList.add('wrapper')
 
@@ -32,9 +40,46 @@ apps.innerHTML = 'Apps'
 entrees.innerHTML = 'Entrees'
 desserts.innerHTML = 'Desserts'
 
+apps.appendChild(appsList)
+entrees.appendChild(entreesList)
+desserts.append(dessertsList)
+
+
 wrapper.appendChild(apps)
 wrapper.appendChild(entrees)
 wrapper.appendChild(desserts)
 
 insert.appendChild(wrapper)
+
+const links = document.querySelectorAll('.page-link');
+const sections = document.querySelectorAll('section');
+
+function highlightLink() {
+  // Get the current scroll position
+  const scrollPosition = window.scrollY;
+
+  // Find the section that is currently in view
+  let currentSection = null;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+      currentSection = section;
+    }
+  });
+
+  // Remove active class from all links
+  links.forEach((link) => link.classList.remove('active'));
+
+  // Add active class to the corresponding link
+  const currentLinkId = currentSection ? currentSection.id : '';
+  const currentLink = document.querySelector(`a[href="#${currentLinkId}"]`);
+  if (currentLink) {
+    currentLink.classList.add('active');
+  }
+}
+
+// Attach scroll event listener to the window
+window.addEventListener('scroll', highlightLink);
   }
